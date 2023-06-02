afa9ea78adb70b9ce62a49e04ac6701f34c3c7cc
Marko Mijatović <mijat5555@gmail.com>
2023-06-02 10:32:20 +0200

    brisanje SocialNetwork_2 nije potreban vise

--- SocialNetwork_2/socialNetwork_SaPočetnomStranicom_Faker/app/Controllers/Http/HomeController.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Post from 'App/Models/Post'
-
-export default class HomeController {
-    public async index({ view }: HttpContextContract){
-        const posts = await Post.query().preload('user')
-        return view.render('welcome', { posts })
-    }
-}
